e466edf5cf068c51160cfb40f24a0e7493435b48
harithaselvakumaran16 <125334624+harithaselvakumaran16@users.noreply.github.com>
2023-08-15 22:38:08 -0400

    Update relational_layers.py

--- disentanglement_lib/evaluation/abstract_reasoning/relational_layers.py
+++ disentanglement_lib/evaluation/abstract_reasoning/relational_layers.py
@@ -19,10 +19,11 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import tensorflow.compat.v1 as tf
+#import tensorflow.compat.v1 as tf
+import torch
 
 
-class RelationalLayer(tf.keras.layers.Layer):
+class RelationalLayer(nn.Module):
   """Implements a single relational layer.
 
   This layer is based on equation (1) of https://arxiv.org/pdf/1706.01427.pdf
@@ -60,23 +61,22 @@ class RelationalLayer(tf.keras.layers.Layer):
     return outputs
 
 
-class PairwiseEdgeEmbeddings(tf.keras.layers.Layer):
+class PairwiseEdgeEmbeddings(nn.Module):
   """Creates pairwise edge embeddings from node embeddings."""
 
   def call(self, inputs, **kwargs):
     num_nodes = inputs.get_shape().as_list()[-2]
-    return tf.concat(
-        (repeat(tf.expand_dims(inputs, -2), num_nodes, -2),
-         repeat(tf.expand_dims(inputs, -3), num_nodes, -3)),
-        axis=-1)
+    concatenated = torch.cat((inputs.unsqueeze(-2).expand(-1, num_nodes, -1, -1), 
+                              inputs.unsqueeze(-3).expand(-1, -1, num_nodes, -1)), 
+                             dim=-1)
+    return concatenated
 
 
 def repeat(tensor, num, axis):
   """Repeats tensor num times along the specified axis."""
   multiples = [1] * tensor.get_shape().ndims
   multiples[axis] = num
-  return tf.tile(tensor, multiples)
-
+  return tensor.repeat(*multiples)
 
 def positional_encoding_like(tensor, positional_encoding_axis=-2,
                              value_axis=-1):
@@ -101,27 +101,32 @@ def positional_encoding_like(tensor, positional_encoding_axis=-2,
     Positional encoding tensor of the same dtype as tensor.
   """
   # First, create identity matrix for one-hot embedding.
-  shape = tensor.get_shape()
-  num_values = shape.as_list()[positional_encoding_axis]
-  result = tf.eye(num_values, dtype=tensor.dtype)
+  shape = tensor.size()
+  num_values = shape[positional_encoding_axis]
+  result = torch.eye(num_values, dtype=tensor.dtype, device=tensor.device)
+    
   # Second, reshape to proper dimensionality.
-  new_shape = [1] * shape.ndims
+  new_shape = [1] * len(shape)
   new_shape[positional_encoding_axis] = num_values
   new_shape[value_axis] = num_values
-  result = tf.reshape(result, new_shape)
+  result = result.view(*new_shape)
+                               
   # Third, broadcast to final shape.
-  multiplier = shape.as_list()
+  multiplier = list(shape)
   multiplier[positional_encoding_axis] = 1
   multiplier[value_axis] = 1
-  if not shape.is_fully_defined():
-    dynamic_shape = tf.unstack(tf.shape(tensor))
-    multiplier = [
-        (dynamic_shape[i] if n is None else n) for i, n in enumerate(multiplier)
-    ]
-  return tf.tile(result, multiplier)
+
+  if None in shape:
+        dynamic_shape = list(tensor.size())
+        for i, n in enumerate(multiplier):
+            if n is None:
+                multiplier[i] = dynamic_shape[i]
+                
+    result = result.repeat(*multiplier)
+    return result
 
 
-class AddPositionalEncoding(tf.keras.layers.Layer):
+class AddPositionalEncoding(nn.Module):
   """Adds positional encoding to embedding based on position along axis.
 
   Consider a tensor of shape (batch_size, num_pos, num_dim). This layer will
@@ -142,10 +147,10 @@ class AddPositionalEncoding(tf.keras.layers.Layer):
         inputs,
         positional_encoding_axis=self.positional_encoding_axis,
         value_axis=self.embedding_axis)
-    return tf.concat([inputs, onehot_embedding], axis=self.embedding_axis)
+    return torch.cat([inputs, onehot_embedding], axis=self.embedding_axis)
 
 
-class StackAnswers(tf.keras.layers.Layer):
+class StackAnswers(nn.Module):
   """Concatenates each each answer with the context and stacks them."""
 
   def __init__(self, answer_axis=-2, stack_axis=-3):
@@ -158,13 +163,13 @@ class StackAnswers(tf.keras.layers.Layer):
     num_answers = answers.get_shape().as_list()[self.answer_axis]
     answer_blocks = []
     for i in range(num_answers):
-      ith_answer = tf.gather(answers, [i], axis=self.answer_axis)
-      ith_answer_block = tf.concat([context, ith_answer], axis=self.answer_axis)
+      ith_answer = torch.gather(answers, [i], axis=self.answer_axis)
+      ith_answer_block = torch.cat([context, ith_answer], axis=self.answer_axis)
       answer_blocks.append(ith_answer_block)
-    return tf.stack(answer_blocks, axis=self.stack_axis)
+    return torch.stack(answer_blocks, axis=self.stack_axis)
 
 
-class MultiDimBatchApply(tf.keras.layers.Layer):
+class MultiDimBatchApply(nn.Module):
   """Applies layer for each element in a multi-dimensional batch."""
 
   def __init__(self, layer, num_dims_to_keep=3):
@@ -184,9 +189,9 @@ class MultiDimBatchApply(tf.keras.layers.Layer):
   def call(self, inputs, **kwargs):
     shape = inputs.get_shape().as_list()
     collapsed_shape = [-1] + shape[-self.num_dims_to_keep:]
-    inputs = tf.reshape(inputs, collapsed_shape)
+    inputs = torch.reshape(inputs, collapsed_shape)
     # Apply the layer.
     output = self.layer(inputs, **kwargs)
     active_shape = output.get_shape().as_list()[1:]
     output_shape = [-1] + shape[1:-self.num_dims_to_keep] + active_shape
-    return tf.reshape(output, output_shape)
+    return torch.reshape(output, output_shape)
