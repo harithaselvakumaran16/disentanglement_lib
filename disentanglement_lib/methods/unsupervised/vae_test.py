eb48d997587cf784bd0a1294431a83fbd39e0cec
harithaselvakumaran16 <125334624+harithaselvakumaran16@users.noreply.github.com>
2023-08-16 21:37:09 -0400

    Update vae_test.py

--- disentanglement_lib/methods/unsupervised/vae_test.py
+++ disentanglement_lib/methods/unsupervised/vae_test.py
@@ -20,22 +20,24 @@ from __future__ import print_function
 from absl.testing import parameterized
 from disentanglement_lib.methods.unsupervised import vae
 import numpy as np
-import tensorflow.compat.v1 as tf
-import gin.tf.external_configurables  # pylint: disable=unused-import
-
+#import tensorflow.compat.v1 as tf
+import torch
+import gin.torch.external_configurables  # pylint: disable=unused-import
+from torch.testing import torch_testing
+import unittest
 
 def _make_symmetric_psd(matrix):
   return 0.5 * (matrix + matrix.T) + np.diag(np.ones(10)) * 10.
 
 
-class VaeTest(parameterized.TestCase, tf.test.TestCase):
+class VaeTest(parameterized.TestCase, torch_testing.TestCase):
 
   @parameterized.parameters((np.zeros([10, 10]), np.zeros([10, 10]), 0., 0.01),
                             (np.ones([10, 10]), np.zeros([10, 10]), 5., 5.01),
                             (np.ones([10, 10]), np.ones([10, 10]), 8.58, 8.6))
   def test_compute_gaussian_kl(self, mean, logvar, target_low, target_high):
-    mean_tf = tf.convert_to_tensor(mean, dtype=np.float32)
-    logvar_tf = tf.convert_to_tensor(logvar, dtype=np.float32)
+    mean_tf = torch.tensor(mean, dtype=np.float32)
+    logvar_tf = torch.tensor(logvar, dtype=np.float32)
     with self.test_session() as sess:
       test_value = sess.run(vae.compute_gaussian_kl(mean_tf, logvar_tf))
       self.assertBetween(test_value, target_low, target_high)
@@ -55,7 +57,7 @@ class VaeTest(parameterized.TestCase, tf.test.TestCase):
       (False, np.zeros(10), _make_symmetric_psd(np.random.random((10, 10)))))
   def test_compute_covariance_z_mean(self, diagonal, mean, cov):
     if diagonal:
-      samples = tf.random.normal(
+      samples = torch.normal(
           shape=(100000, 10), mean=mean, stddev=tf.math.sqrt(cov))
       cov = np.diag(np.ones([10])) * cov
     else:
@@ -69,14 +71,14 @@ class VaeTest(parameterized.TestCase, tf.test.TestCase):
       (np.ones([10, 10]), 90., 90.1), (np.zeros([10, 10]), 10., 10.1),
       (np.diag(np.ones(10)), 0., 0.1), (2. * np.diag(np.ones(10)), 10., 10.1))
   def test_regularize_diag_off_diag_dip(self, matrix, target_low, target_high):
-    matrix_tf = tf.convert_to_tensor(matrix, dtype=np.float32)
+    matrix_tf = torch.tensor(matrix, dtype=np.float32)
     with self.test_session() as sess:
       test_value = sess.run(vae.regularize_diag_off_diag_dip(matrix_tf, 1, 1))
       self.assertBetween(test_value, target_low, target_high)
 
   @parameterized.parameters((0., -1.4190, -1.4188), (1., -0.92, -0.91))
   def test_gaussian_log_density(self, z_mean, target_low, target_high):
-    matrix = tf.ones(1)
+    matrix = torch.ones(1)
     with self.test_session() as sess:
       test_value = sess.run(vae.gaussian_log_density(matrix, z_mean, 0.))[0]
       self.assertBetween(test_value, target_low, target_high)
@@ -85,13 +87,13 @@ class VaeTest(parameterized.TestCase, tf.test.TestCase):
       (1, 0., 0.1), (10, -82.9, -82.89))  # -82.893 = (10 - 1) * ln(10000)
   def test_total_correlation(self, num_dim, target_low, target_high):
     # Since there is no dataset, the constant should be (num_latent - 1)*log(N)
-    z = tf.random.normal(shape=(10000, num_dim))
-    z_mean = tf.zeros(shape=(10000, num_dim))
-    z_logvar = tf.zeros(shape=(10000, num_dim))
+    z = torch.normal(shape=(10000, num_dim))
+    z_mean = torch.zeros(shape=(10000, num_dim))
+    z_logvar = torch.zeros(shape=(10000, num_dim))
     with self.test_session() as sess:
       test_value = sess.run(vae.total_correlation(z, z_mean, z_logvar))
       self.assertBetween(test_value, target_low, target_high)
 
 
 if __name__ == "__main__":
-  tf.test.main()
+  unittest.main()
